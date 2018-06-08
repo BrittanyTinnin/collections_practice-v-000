efbcdb8f237176b0e42dd8e5f5fa35c887dc1d9b
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-08 18:48:18 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+require 'pry'
 
+def sort_array_asc(integers)
+  integers.sort
+end
+
+def sort_array_desc(integers)
+  integers.sort { |a,b| b <=> a }
+end
+
+def sort_array_char_count(integers)
+  integers.sort { |a,b| a.length <=> b.length }
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(integers)
+  integers.reverse
+end
+
+def kesha_maker(array)
+  array.each do |word|
+  
+    
+  end
+end
